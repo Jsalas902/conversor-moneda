1a72dd357d664d2dae6eafb5a4c0bd98d0bd3499
jainner salas <jsalas902@gmail.com>
2019-08-11 02:50:42 -0400

    creando los methods, computed, watch necesarios para el conversor de monedas

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # conversor-moneda
+
+![screensot pokemon](images/screenshot-demo.png "Screenshot Pokemon")
+_Simplemente abra index.html_
+
+## Construido con 🛠️
+
+_Herramientas utilizadas para crear el proyecto_
+
+* [JS](https://vuejs.org/) - El framework Vue
+* [AXIOS](https://www.json.org/) - El back end consumido el API https://www.currencyconverterapi.com
+* [HTML](#) - HyperText Markup Language
+* [CSS](https://getbootstrap.com/) - Bootstrap
+
+---
+⌨️ con ❤️ por [Jsalas](https://github.com/Jsalas902) 😊
--- app.js
+++ app.js
@@ -1,13 +1,60 @@
 new Vue({
     el:'#app',
     data: {
-        monedas: []
+        monedas: [],
+        cantidad: 0,
+        from: 'EUR',
+        to: 'USD',
+        result: 0
     },
     mounted() {
-        axios.get('https://free.currconv.com/api/v7/currencies?apiKey=9e3a780ed9f9a73733da')
+        this.getMonedas()
+    },
+    computed: {
+        formatearMonedas() {
+            return Object.values(this.monedas);
+        },
+        calcularResultado() {
+            return (Number(this.cantidad) * this.result).toFixed(3);
+        },
+        desabilitado() {
+            return this.cantidad === 0 || ! this.cantidad;
+        }
+    },
+    methods: {
+        getMonedas() {
+            const datos  = localStorage.getItem('monedas');
+
+            if (datos) {
+                this.monedas = JSON.parse(datos);
+                return;
+            }
+
+            axios.get('https://free.currconv.com/api/v7/currencies?apiKey=9e3a780ed9f9a73733da')
             .then(response => {
                 this.monedas = response.data.results;
+                localStorage.setItem('monedas', JSON.stringify(response.data.results));
+                // console.log(response);
+            }); 
+        },
+        convertirMonedas() {
+            const busqueda = `${this.from}_${this.to}`;
+
+            axios.get(`https://free.currconv.com/api/v7/convert?q=${busqueda}&apiKey=9e3a780ed9f9a73733da`)
+            .then(response => {
                 console.log(response);
+                this.result = response.data.results[busqueda].val;
             });
+        }
     },
+
+    watch: {
+        from() {
+            this.result = 0;
+        },
+        to() {
+            this.result = 0; 
+        }
+    }
+
 });
